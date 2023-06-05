f91910acf4f16351e7f82fc906a8e7b87c97eaac
munenecarl <munenecarl@gmail.com>
2023-06-05 21:43:52 +0300

    correction to 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(", ")[-1].split(" ")[0] + "-" + str.split(" ")[-2] + " " + str.split(" ")[-1])
+str  str[39:67] + str[107:112] + str[:6]=
 print(str)
