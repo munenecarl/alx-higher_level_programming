0301a68aded2df7a95a90260b45ac525925fb194
munenecarl <munenecarl@gmail.com>
2023-10-18 11:35:00 +0300

    corrections to fifth file

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE, COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
