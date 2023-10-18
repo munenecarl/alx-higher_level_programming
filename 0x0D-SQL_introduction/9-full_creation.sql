15cda64b6f4e42c4293e33ed27482afab5a298af
munenecarl <munenecarl@gmail.com>
2023-10-18 11:46:25 +0300

    corrections to ninth file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- script that creates the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
