3766f32ea3f32f0de26130077c6ebd1c5b8b3a54
munenecarl <munenecarl@gmail.com>
2023-10-18 11:57:52 +0300

    corrections to the fourteenth file

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- Script that computes the average score of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT AVG(score) AS average FROM hbtn_0c_0.second_table;
+SELECT AVG(score) AS average FROM second_table;
