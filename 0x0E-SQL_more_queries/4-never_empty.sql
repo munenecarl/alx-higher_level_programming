0b2f4e5086539fb555688a26b6c45ac785e5ef19
munenecarl <munenecarl@gmail.com>
2023-10-19 15:33:14 +0300

    corrections to fourth file

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -6,5 +6,5 @@
 -- If the table id_not_null already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
-    name VARCHAR(256),
+    name VARCHAR(256)
 );
