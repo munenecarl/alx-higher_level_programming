46d8d0db3130e5f29af56e79b7e5fd5c48950700
munenecarl <munenecarl@gmail.com>
2023-10-19 19:42:18 +0300

    corrections

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,6 +5,6 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id
-  FROM tv_shows, tv_show_genres
- WHERE tv_shows.id = tv_show_genres.tv_show_id
- ORDER BY tv_shows.title, tv_show_genres.genre_id;
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
