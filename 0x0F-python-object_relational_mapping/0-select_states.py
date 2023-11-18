7a3916ad2ba9572832a6d29e441d697cd327f74e
munenecarl <munenecarl@gmail.com>
2023-11-19 01:13:57 +0300

    completed 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states from the database"""
+
+import sys
+import MySQLdb
+
+def main():
+    """lists all states from the database"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+    query_string = "SELECT * FROM states ORDER BY id ASC"
+    cur.execute(query_string)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+This is a project to learn how to implement object relational maaping in python.
