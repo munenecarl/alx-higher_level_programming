72cd4243473535b3902d9191525380406ca8256b
munenecarl <munenecarl@gmail.com>
2023-11-19 01:42:45 +0300

    completed the third task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N) from the database"""
+
+import sys
+import MySQLdb
+
+def main():
+    """lists all states with a name starting with N (upper N) from the database"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    cur = db.cursor()
+    query_string = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    params = (state_name, )
+    cur.execute(query_string, params)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
