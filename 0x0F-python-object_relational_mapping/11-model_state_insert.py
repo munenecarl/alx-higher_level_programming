283b32ec2e60f27fb52d5950c5f4aa5ec2d2fae7
munenecarl <munenecarl@gmail.com>
2023-11-19 18:41:36 +0300

    completed twelth task

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import State
+
+def main():
+    """lists the State object with the name passed as argument"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name= 'Louisiana')
+    session.add(new_state)
+    session.commit()
+    state = session.query(State).order_by(State.id.desc()).first()
+    print("{}".format(state.id))
+
+if __name__ == "__main__":
+    main()
