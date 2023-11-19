c5f4b3c24a238e11d31b70d6052b5941835f64f1
munenecarl <munenecarl@gmail.com>
2023-11-19 14:29:10 +0300

    completed 7th tasks

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class definition of a State and an instance Base"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Defines the attributes of state table
+    Attributes:
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key= True)
+    name = Column(String(128), nullable= False)
