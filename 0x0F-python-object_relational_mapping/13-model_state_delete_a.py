bde6b25d62c759bb2960cb08b21d0ed4aa43f634
munenecarl <munenecarl@gmail.com>
2023-11-19 19:39:35 +0300

    completed the fourteenth task

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument"""
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import State
+
+def main():
+    """lists the State object with the name passed as argument"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(State).filter(State.name.like('%a%')).delete()
+    session.commit()
+
+if __name__ == "__main__":
+    main()
