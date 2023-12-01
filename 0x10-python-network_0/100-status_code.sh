90b645a4efec1dcc61929ad2c9e00498775a7ea5
munenecarl <munenecarl@gmail.com>
2023-12-01 18:15:13 +0300

    corrections to shebang

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#!/bin/bas
+#!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response. No pipes, redirections, etc.
 curl -s -o /dev/null -w "%{http_code}" "$1"
