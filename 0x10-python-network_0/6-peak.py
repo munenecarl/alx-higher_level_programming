94d62f83bc1808dcef2491d80726efa1b8a440c0
munenecarl <munenecarl@gmail.com>
2023-12-02 23:14:56 +0300

    correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,6 @@
-#!usr/bin/python3
-""" Module that finds a peak in a list of unsorted integers """
+#!/bin/env python3
 
+""" Module that finds a peak in a list of unsorted integers """
 
 def find_peak(list_of_integers):
     """function that finds a peak in a list of unsorted integers."""
