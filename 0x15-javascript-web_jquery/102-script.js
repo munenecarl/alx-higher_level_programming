399825fa2574afbbbdfa456bf73266bc455dc5ef
munenecarl <munenecarl@gmail.com>
2024-01-11 00:47:41 +0300

    Add advanced task #3

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#btn_translate').click(function(){
+        const lang = $('#language_code').val();
+        $.getJSON('https://www.fourtonfish.com/hellosalut/hello/', { lang: lang }, function(data){
+            $('#hello').text(data.hello);
+        });
+    });
+});
